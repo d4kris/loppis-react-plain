dacedc2365f3c7e85a0996186d17e3e16d7db630
Kris Luhr <kristin@liberger.com>
2016-02-18 18:03:18 +0100

    NewItem component

--- js/Purchases.js
+++ js/Purchases.js
@@ -20,25 +20,18 @@ var Purchase = React.createClass({
   }
 });
 
-var Purchases = React.createClass({
-  displayName: "Purchases",
-
-  getInitialState: function () {
-    return {
-      list: []
-    };
-  },
+var NewItem = React.createClass({
+  displayName: "NewItem",
 
   add: function (e) {
     e.preventDefault();
-    console.log('add');
+    console.log('add NewItem');
     var newItem = {
       key: +new Date(),
       seller: this.refs.seller.value,
       price: this.refs.price.value
     };
-    var items = [newItem].concat(this.state.list);
-    this.setState({ list: items });
+    this.props.onAdd(newItem);
     // clear form
     this.refs.seller.value = '';
     this.refs.price.value = '';
@@ -49,34 +42,54 @@ var Purchases = React.createClass({
   render: function () {
     return React.createElement(
       "div",
-      { className: "container" },
+      { className: "add row" },
       React.createElement(
-        "div",
-        { className: "add row" },
+        "form",
+        { role: "form", className: "" },
         React.createElement(
-          "form",
-          { role: "form", className: "" },
+          "div",
+          { className: "col-xs-5 col-md-2" },
+          React.createElement("input", { type: "number", name: "seller", ref: "seller", placeholder: "Sälj nr", className: "form-control" })
+        ),
+        React.createElement(
+          "div",
+          { className: "col-xs-5 col-md-3" },
+          React.createElement("input", { type: "number", ref: "price", step: "5", placeholder: "Pris", className: "form-control" })
+        ),
+        React.createElement(
+          "div",
+          { className: "col-xs-2 col-md-1" },
           React.createElement(
-            "div",
-            { className: "col-xs-5 col-md-2" },
-            React.createElement("input", { type: "number", name: "seller", ref: "seller", placeholder: "Sälj nr", className: "form-control" })
-          ),
-          React.createElement(
-            "div",
-            { className: "col-xs-5 col-md-3" },
-            React.createElement("input", { type: "number", ref: "price", step: "5", placeholder: "Pris", className: "form-control" })
-          ),
-          React.createElement(
-            "div",
-            { className: "col-xs-2 col-md-1" },
-            React.createElement(
-              "button",
-              { onClick: this.add, className: "btn btn-success" },
-              React.createElement("i", { className: "glyphicon glyphicon-plus" })
-            )
+            "button",
+            { onClick: this.add, className: "btn btn-success" },
+            React.createElement("i", { className: "glyphicon glyphicon-plus" })
           )
         )
-      ),
+      )
+    );
+  }
+});
+
+var Purchases = React.createClass({
+  displayName: "Purchases",
+
+  getInitialState: function () {
+    return {
+      list: []
+    };
+  },
+
+  add: function (newItem) {
+    console.log('add to list');
+    var items = [newItem].concat(this.state.list);
+    this.setState({ list: items });
+  },
+
+  render: function () {
+    return React.createElement(
+      "div",
+      { className: "container" },
+      React.createElement(NewItem, { onAdd: this.add }),
       this.state.list.map(function (p) {
         return React.createElement(Purchase, { key: p.key, seller: p.seller, price: p.price });
       })
--- jsx/Purchases.jsx
+++ jsx/Purchases.jsx
@@ -13,23 +13,16 @@ var Purchase = React.createClass({
   }
 });
 
-var Purchases = React.createClass({
-  getInitialState: function () {
-    return {
-      list: []
-    };
-  },
-
+var NewItem = React.createClass({
   add: function (e) {
     e.preventDefault();
-    console.log('add');
+    console.log('add NewItem');
     var newItem = {
       key: +new Date(),
       seller : this.refs.seller.value,
       price: this.refs.price.value
     };
-    var items = [newItem].concat(this.state.list);
-    this.setState({ list : items });
+    this.props.onAdd(newItem);
     // clear form
     this.refs.seller.value = '';
     this.refs.price.value = '';
@@ -37,23 +30,41 @@ var Purchases = React.createClass({
     this.refs.seller.focus();
   },
 
+  render: function () {
+    return <div className="add row">
+      <form role="form" className="">
+        <div className="col-xs-5 col-md-2">
+          <input type="number" name="seller" ref="seller" placeholder="Sälj nr" className="form-control"/>
+        </div>
+        <div className="col-xs-5 col-md-3">
+          <input type="number" ref="price" step="5" placeholder="Pris" className="form-control"/>
+        </div>
+        <div className="col-xs-2 col-md-1">
+          <button onClick={this.add} className="btn btn-success">
+            <i className="glyphicon glyphicon-plus"></i>
+          </button>
+        </div>
+      </form>
+    </div>;
+  }
+});
+
+var Purchases = React.createClass({
+  getInitialState: function () {
+    return {
+      list: []
+    };
+  },
+
+  add: function (newItem) {
+    console.log('add to list');
+    var items = [newItem].concat(this.state.list);
+    this.setState({ list : items });
+  },
+
   render : function () {
     return <div className="container">
-      <div className="add row">
-        <form role="form" className="">
-          <div className="col-xs-5 col-md-2">
-            <input type="number" name="seller" ref="seller" placeholder="Sälj nr" className="form-control"/>
-          </div>
-          <div className="col-xs-5 col-md-3">
-            <input type="number" ref="price" step="5" placeholder="Pris" className="form-control"/>
-          </div>
-          <div className="col-xs-2 col-md-1">
-            <button onClick={this.add} className="btn btn-success">
-              <i className="glyphicon glyphicon-plus"></i>
-            </button>
-          </div>
-        </form>
-      </div>
+      <NewItem onAdd={this.add}/>
       {this.state.list.map(function (p) {
         return <Purchase key={p.key} seller={p.seller} price={p.price} />;
       })}
